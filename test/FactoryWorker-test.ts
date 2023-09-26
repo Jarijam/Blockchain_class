13031bffaf396e463c6b5c758b570136b68352b1
Jeongwon Lee <85335101+Jarijam@users.noreply.github.com>
2023-09-26 15:28:17 +0900

    20230926

--- test/FactoryWorker-test.ts
+++ test/FactoryWorker-test.ts
@@ -1,5 +1,11 @@
 import { expect } from "chai";
-import { Contract, getRandomNonce, Signer, toNano, WalletTypes } from "locklift";
+import {
+  Contract,
+  getRandomNonce,
+  Signer,
+  toNano,
+  WalletTypes,
+} from "locklift";
 import { FactorySource } from "../build/factorySource";
 import { Account } from "everscale-standalone-client";
 
@@ -8,6 +14,7 @@ let workers: Contract<FactorySource["Workers"]>;
 
 let factoryAccount: Account;
 let workerAccount: Account;
+let workerAccount2: Account;
 let factoryWallet: Account;
 let signer: Signer;
 
@@ -22,7 +29,7 @@ describe("Car_Factory_Test", async function () {
       workerAccount = await locklift.factory.accounts
         .addNewAccount({
           type: WalletTypes.EverWallet,
-          value: toNano(1),
+          value: toNano(10),
           publicKey: signer.publicKey,
         })
         .then(res => res.account);
@@ -35,7 +42,8 @@ describe("Car_Factory_Test", async function () {
         })
         .then(res => (factoryWallet = res.account));
 
-      const { code: workerCode } = locklift.factory.getContractArtifacts("Workers");
+      const { code: workerCode } =
+        locklift.factory.getContractArtifacts("Workers");
       carFactory = await locklift.factory
         .deployContract({
           contract: "CarFactory",
@@ -45,22 +53,35 @@ describe("Car_Factory_Test", async function () {
           constructorParams: {
             _owner: factoryWallet.address,
           },
-          value: toNano(1),
+          value: toNano(10),
           publicKey: signer.publicKey,
         })
         .then(res => res.contract);
-
-      // worker = await locklift.factory
+      expect(
+        await locklift.provider
+          .getBalance(workerAccount.address)
+          .then(balance => Number(balance)),
+      ).to.be.above(0);
     });
     it("Interact with contract", async function () {
-      const workerAddress = await carFactory.methods.getAddress({ _accountAddress: workerAccount.address }).call();
+      const { traceTree: deployWorker } = await locklift.tracing.trace(
+        carFactory.methods
+          .deployAccountForWorker({
+            _initBalance: 10,
+          })
+          .send({ from: workerAccount.address, amount: toNano(10) }),
+      );
+      deployWorker?.beautyPrint();
 
-      console.log(workerAddress);
+      const event = deployWorker?.findEventsForContract({
+        contract: carFactory,
+        name: "AccountDeployed" as const,
+      })[0]!;
 
       const { traceTree: sendOffer } = await locklift.tracing.trace(
         carFactory.methods
           .sendOffer({
-            _destAddress: workerAccount.address,
+            _accountAddress: workerAccount.address,
             offer: "",
           })
           .send({
@@ -75,6 +96,23 @@ describe("Car_Factory_Test", async function () {
       );
       await sendOffer?.beautyPrint();
 
+      let { accountAddress } = event;
+      const workerContract = locklift.factory.getDeployedContract(
+        "Workers",
+        accountAddress,
+      );
+
+      const { traceTree: sendAnswer } = await locklift.tracing.trace(
+        workerContract.methods
+          .sendOfferAnswer({
+            _answer: "YES",
+          })
+          .send({
+            from: workerAccount.address,
+            amount: toNano(1),
+          }),
+      );
+      sendAnswer?.beautyPrint();
       // await carFactory.methods
       //   .sendOffer({
       //     _destAddress: workers.address,
